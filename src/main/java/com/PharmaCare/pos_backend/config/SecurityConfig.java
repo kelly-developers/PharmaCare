0e2e0acf11a96a927ba8bd1302531a2b457e9f5f
Kelly Nyachiro <kellynyachiro@gmail.com>
2025-12-20 10:14:53 +0300

    latest

--- src/main/java/com/PharmaCare/pos_backend/config/SecurityConfig.java
+++ src/main/java/com/PharmaCare/pos_backend/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("https://pharmacares.netlify.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
