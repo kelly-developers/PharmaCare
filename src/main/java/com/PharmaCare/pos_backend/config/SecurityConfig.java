d3d4199dbe7bbc35da830f53e77c23ca5e1ba163
Kelly Nyachiro <kellynyachiro@gmail.com>
2025-12-20 10:47:29 +0300

    latest

--- src/main/java/com/PharmaCare/pos_backend/config/SecurityConfig.java
+++ src/main/java/com/PharmaCare/pos_backend/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
