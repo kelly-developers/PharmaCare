29df928ac1ab5447d167bb2d568e636ed319150a
Kelly Nyachiro <kellynyachiro@gmail.com>
2025-12-23 13:44:11 +0300

    latest

--- src/main/java/com/PharmaCare/pos_backend/enums/Role.java
+++ src/main/java/com/PharmaCare/pos_backend/enums/Role.java
@@ -1,8 +1,15 @@
 package com.PharmaCare.pos_backend.enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Role {
     ADMIN,
     MANAGER,
     PHARMACIST,
-    CASHIER
+    CASHIER;
+
+    @JsonValue
+    public String getValue() {
+        return this.name().toLowerCase();
+    }
 }
