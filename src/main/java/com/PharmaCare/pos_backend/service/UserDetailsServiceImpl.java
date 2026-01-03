263bac53644bcfa8bf323b33572c4c468cfe5272
Kelly Nyachiro <kellynyachiro@gmail.com>
2026-01-03 09:27:21 +0300

    latest

--- src/main/java/com/PharmaCare/pos_backend/service/UserDetailsServiceImpl.java
+++ src/main/java/com/PharmaCare/pos_backend/service/UserDetailsServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service("userDetailsService")  // Explicit bean name
+@Service("userDetailsService")  
 @RequiredArgsConstructor
 @Slf4j
 public class UserDetailsServiceImpl implements UserDetailsService {
